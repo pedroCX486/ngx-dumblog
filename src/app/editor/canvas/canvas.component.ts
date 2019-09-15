04737e18a1a7a433268a0ec436804bdf933b6d56
Pedro Morais <pedrocx486@gmail.com>
2019-09-15 15:42:27 -0300

    Change file structure, add mocks and create initial file loading structure on the editor's code

--- src/app/editor/canvas/canvas.component.ts
+++ src/app/editor/canvas/canvas.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import * as DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 import { saveAs } from 'file-saver';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-canvas',
@@ -10,37 +13,53 @@ import { saveAs } from 'file-saver';
 export class CanvasComponent implements OnInit {
 
   postTitle = '';
-
   Editor = DecoupledEditor;
-  editorData = 'Hello World';
-
+  editorData = 'A new post...';
   filename;
+  archives;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() { }
 
-  onReady( editor ) {
+  loadArchive(){
+    this.getJSON('archive').subscribe(data => {
+      this.archives = data;
+    });
+  }
+
+  loadPost(file){
+    this.getJSON(file).subscribe(data => {
+      this.editorData = data.content;
+      this.postTitle = data.title;
+    });
+  }
+
+  saveAll(){
+    saveAs(new Blob([this.editorData], {type: "text/plain;charset=utf-8;"}), this.parseFilename(this.postTitle) + ".json");
+  }
+
+  editorReady(editor) {
     editor.ui.getEditableElement().parentElement.insertBefore(
-        editor.ui.view.toolbar.element,
-        editor.ui.getEditableElement()
+      editor.ui.view.toolbar.element,
+      editor.ui.getEditableElement()
     );
   }
 
-  save(){
-    saveAs(new Blob([this.editorData], {type: "text/plain;charset=utf-8;"}), this.createFilename(this.postTitle) + ".json");
+  getJSON(arg): Observable<any> {
+    return this.http.get("./assets/posts/"+ arg +".json");
   }
 
-  createFilename(postTitle){
+  parseFilename(postTitle){
     var filename = postTitle;
 
     if(filename == null || filename == ''){
-      filename = "No Title";
-    } 
+      return filename = "no-title";
+    }
 
     filename = filename.replace(/[^a-zA-Z0-9_]+/gi, "-").toLowerCase();
 
-    if(filename.endsWith("-"))
+    while(filename.endsWith("-"))
     {
       filename = filename.slice(0, -1);
     }
