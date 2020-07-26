418c76b58e2b914ff1bf9aae5bc5556996e36fd5
Pedro Morais <pedrocx486@gmail.com>
2020-07-26 20:07:53 -0300

    Package update and models cleanup

--- src/shared/models/archive.model.ts
+++ src/shared/models/archive.model.ts
@@ -1,5 +1,11 @@
 export class ArchiveModel {
-    postTitle = '';
-    timestamp = '';
-    filename = '';
+    postTitle: string;
+    timestamp: string;
+    filename: string;
+
+    constructor(archive?: ArchiveModel) {
+        this.postTitle = archive?.postTitle || '';
+        this.timestamp = archive?.timestamp || '';
+        this.filename = archive?.filename || '';
+    }
 }
--- src/shared/models/post.model.ts
+++ src/shared/models/post.model.ts
@@ -1,8 +1,17 @@
 export class PostModel {
-    postTitle = '';
-    timestamp = '';
-    editedTimestamp = '';
-    postContent = '';
-    filename = '';
-    draft = false;
+    postTitle: string;
+    timestamp: string;
+    editedTimestamp: string;
+    postContent: string;
+    filename: string;
+    draft: boolean;
+
+    constructor(post?: PostModel) {
+        this.postTitle = post?.postTitle || '';
+        this.timestamp = post?.timestamp || '';
+        this.editedTimestamp = post?.editedTimestamp || '';
+        this.postContent = post?.postContent || '';
+        this.filename = post?.filename || '';
+        this.draft = post?.draft || false;
+    }
 }
